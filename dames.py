4ea866e3c053e9ab9485da071962836bea9e5de1
SISBANE ISSAM p1906661 <issam.sisbane@etu.univ-lyon1.fr>
2021-01-13 15:13:59 +0100

    ajout du projet

---
+++ dames.py
@@ -0,0 +1,167 @@
+﻿import pygame
+from pygame.locals import *
+from tkinter import *
+import os
+
+def casecliquée(point):
+    case=[0,0]
+    case[0]=int((point[0]-5)/50)
+    case[1]=int((point[1]-8)/50)
+    return case
+
+def detectionfin(plateau):
+    fin=0
+    for i in range(10):
+        for j in range(10):
+            if plateau[i][j]!=0:
+                fin=1
+    return fin
+
+def deplactsimple(colonnedepart,lignedepart,colonnearrivee,lignearrivee,sens,plateau):
+    if lignedepart==lignearrivee-sens and colonnearrivee==colonnedepart and plateau[colonnearrivee][lignearrivee]==0:
+        return 1
+    else:
+        return 0
+
+
+def depactprisepion(colonnedepart,lignedepart,colonnearrivee,lignearrivee,plateau,pion):
+    depvalide=0
+    if lignedepart==lignearrivee and colonnearrivee-colonnedepart==2 and plateau[colonnedepart+1][lignearrivee]!=0 and plateau[colonnedepart+1][lignearrivee]!=pion:
+        depvalide=1
+    if lignedepart==lignearrivee and colonnearrivee-colonnedepart==-2 and plateau[colonnedepart-1][lignearrivee]!=0 and plateau[colonnedepart-1][lignearrivee]!=pion:
+        depvalide=1
+    if lignedepart-lignearrivee==2 and colonnearrivee==colonnedepart and plateau[colonnedepart][lignearrivee+1]!=0 and plateau[colonnedepart][lignearrivee+1]!=pion:
+        depvalide=1
+    if lignedepart-lignearrivee==-2 and colonnearrivee==colonnedepart and plateau[colonnedepart][lignearrivee-1]!=0 and plateau[colonnedepart][lignearrivee-1]!=pion:
+        depvalide=1
+    return(depvalide)
+
+
+pygame.init()
+#Création du plateau de jeu
+plateau=[[0 for i in range(10)]for j in range(10)]
+#On met les pions blancs dans le tableau
+for i in range(2):
+    for j in range(5):
+        plateau[2*j][2*i]=1
+        plateau[2*j+1][2*i+1]=1
+#On met les pions noirs dans le tableau
+for i in range(2):
+    for j in range(5):
+        plateau[2*j][9-2*i]=2
+        plateau[2*j+1][8-2*i]=2
+print(plateau)
+
+
+#Ouverture de la fenÃƒÂªtre Pygame
+fenetre = pygame.display.set_mode((500, 600),RESIZABLE)
+
+#Chargement et collage du fond
+fond = pygame.image.load(os.path.dirname( __file__ )+"\\damier.gif")
+fenetre.blit(fond, (0,0))
+bandeau=pygame.image.load(os.path.dirname( __file__ )+"\\bandeau.gif")
+fenetre.blit(bandeau,(0,500))
+
+#Placement des pions
+pion_blanc=pygame.image.load(os.path.dirname( __file__ )+"\\pionblanc.gif")
+pion_blanc.set_colorkey((255,255,255))
+
+pion_noir=pygame.image.load(os.path.dirname( __file__ )+"\\pionnoir.gif")
+pion_noir.set_colorkey((255,255,255))
+for j in range(10):
+    for i in range(10):
+        if plateau[j][i]==1:
+            fenetre.blit(pion_blanc,(j*50+5,i*50+8))
+        elif plateau[j][i]==2:
+            fenetre.blit(pion_noir,(j*50+5,i*50+8))
+
+
+#Chargement et collage du personnage
+
+
+
+
+
+
+#RafraÃƒÂ®chissement de l'ÃƒÂ©cran
+
+
+
+pygame.display.flip()
+
+
+#BOUCLE INFINIE
+continuer = 1
+joueur=1
+while continuer==1:
+    casedepart=[0,0]
+    joueur=(joueur+1)%2
+    if joueur==0:
+        pion=1
+        sens=1
+        pionjoueur=pion_blanc
+    else:
+        pion=2
+        sens=-1
+        pionjoueur=pion_noir
+    print(joueur,pion)
+    fenetre.blit(fond,(0,0))
+    fenetre.blit(bandeau,(0,500))
+    fenetre.blit(pionjoueur,(200,520))
+    for i in range(10):
+        for j in range(10):
+            if plateau[i][j]==1:
+                fenetre.blit(pion_blanc,(i*50+5,j*50+8))
+            elif plateau[i][j]==2:
+                fenetre.blit(pion_noir,(i*50+5,j*50+8))
+    pygame.display.flip()
+    pion_choisi=0
+    while pion_choisi==0:
+        for event in pygame.event.get():
+            if event.type == MOUSEBUTTONDOWN:
+                if event.button == 1 and event.pos[1]<500:
+                    colonne=casecliquée(event.pos)[0]
+                    ligne=casecliquée(event.pos)[1]
+                    if plateau[colonne][ligne]==pion:
+                        pion_choisi=1
+                        colonnecasedepart=colonne
+                        lignecasedepart=ligne
+                        print("pionchoisi",colonnecasedepart,lignecasedepart)
+            if event.type == pygame.QUIT:
+                continuer = 0
+    pion_déplace=0
+    while pion_déplace==0:
+        for event in pygame.event.get():
+            if event.type == MOUSEBUTTONDOWN:
+                if event.button == 1 :
+                    colonne=casecliquée(event.pos)[0]
+                    ligne=casecliquée(event.pos)[1]
+                    print(colonne,ligne)
+                    if deplactsimple(colonnecasedepart,lignecasedepart,colonne,ligne,sens,plateau)==1:
+                        pion_déplace=1
+                        plateau[colonnecasedepart][lignecasedepart]=0
+                        plateau[colonne][ligne]=pion
+                        print("piondeplace",colonne,ligne)
+                    elif depactprisepion(colonnecasedepart,lignecasedepart,colonne,ligne,plateau,pion)==1:
+                        pion_déplace=1
+                        plateau[colonnecasedepart][lignecasedepart]=0
+                        plateau[colonne][ligne]=pion
+                        if colonnecasedepart==colonne and lignecasedepart>ligne :
+                            plateau[colonne][ligne+1]=0
+                        if colonnecasedepart==colonne and lignecasedepart<ligne :
+                            plateau[colonne][ligne-1]=0
+                        if colonnecasedepart>colonne and lignecasedepart==ligne :
+                            plateau[colonne+1][ligne]=0
+                        if colonnecasedepart<colonne and lignecasedepart==ligne :
+                            plateau[colonne-1][ligne]=0
+
+
+                        print("piondeplace",colonne,ligne)
+
+                    else:
+                        print("pas bon")
+            if event.type == pygame.QUIT:
+                continuer = 0
+
+
+pygame.quit()
